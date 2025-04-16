555f1234d7de8f65d010c28dd57dd45c9ca660f3
Mael <mame.duvey@gmail.com>
2025-04-16 15:57:39 +0200

    delete:(user-service)

--- srcs/back/api_gateway/asset/index.ts
+++ srcs/back/api_gateway/asset/index.ts
@@ -1,29 +1,15 @@
 import fastify from 'fastify'
 
-const server = fastify()
-
-server.get('/ping', async (request, reply) => {
-  return 'pong\n'
-})
-
-server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
-})import fastify from 'fastify'
-
-const server = fastify()
+const server = fastify();
 
 server.get('/api/ping', async (request, reply) => {
-  return 'pong\n'
+  return 'pong\n';
 })
 
 server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
+  console.log(`Server listening at ${address}`);
 })
