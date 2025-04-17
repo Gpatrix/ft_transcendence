65eb8ecdb86dd4418b4b1fc8d2858339e3668f03
YoungMame <mame.duvey@gmail.com>
2025-04-17 09:50:05 -0400

    add:(auth-service init)

--- srcs/back/api_gateway/asset/srcs/index.ts
+++ srcs/back/api_gateway/asset/srcs/index.ts
@@ -1,16 +1,43 @@
 import fastify from 'fastify'
+import jwt from 'jsonwebtoken'
 
 const server = fastify();
+const authServiceAddress = "http://auth-service:3001";
 
-server.get('/api/ping', async (request, reply) => {
-  return 'pong\n';
+// server.post<{ Body: { name: string } }>('/api/ping', async (request, reply) => {
+//   console.log(request.body);
+//   reply.send(request.body.name)
+//   return request.body; 
+// })
+
+server.get<{ Params: { userId: string } }>('/api/ping/:userId', async (request, reply) => {
+  const userId = request.params.userId;
+  const response = await fetch(`${authServiceAddress}/${userId}`);
+  if (!response)
+    reply.status(503).send("auth service unavailable");
+  const content = await response.json();
+  if (!content || !content.email || !content.name)
+      reply.status(404).send();
+  const token = await jwt.sign({
+    data: {
+      email: content.email,
+      name: content.name
+    }
+  }, process.env.JWT_SECRET as string, { expiresIn: '24h' });
+  reply.send(token); 
 })
 
-server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-  console.log("Hello World!")
-})
+async function main() {
+  let _address;
+  await server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    _address = address;
+    console.log(`Server listening at ${_address}`);
+  })
+}
+
+main();
+
---
+++ srcs/back/auth-service/asset/srcs/index.ts
@@ -0,0 +1,49 @@
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const server = fastify();
+
+// server.post<{ Body: { name: string } }>('/api/ping', async (request, reply) => {
+//   console.log(request.body);
+//   reply.send(request.body.name)
+//   return request.body;
+// })
+
+server.get<{ Params: { userId: string } }>('/:userId', async (request, reply) => {
+  const userId = Number(request.params.userId);
+  console.log("auth received fetch");
+  const user = await prisma.user.findUnique({
+    where: { 
+      id: userId 
+    }
+  })
+  if (!user)
+    return reply.status(404).send();
+  reply.send(user);
+}) 
+
+async function main() {
+  let _address;
+  await server.listen({ host: '0.0.0.0', port: 3001 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    _address = address;
+    console.log(`Server listening at ${_address}`);
+  })
+}
+
+main();
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
+
