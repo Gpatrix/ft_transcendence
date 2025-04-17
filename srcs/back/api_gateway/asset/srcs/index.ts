2484de26d3df308ee5cf80c228762174f67d00bd
lchauvet <lchauvet@student.42lehavre.fr>
2025-04-17 16:37:21 +0200

    add: simple querry

--- srcs/back/api_gateway/asset/srcs/index.ts
+++ srcs/back/api_gateway/asset/srcs/index.ts
@@ -4,24 +4,42 @@ const APIchat = 'http://chat-service:3000'
 
 const server = fastify();
 
-server.get('/api/chat', async (request, reply) => {
-   try {
-      const response = await fetch(`${APIchat}/test`);
-      const result = await response.json();
-      return (result);
-   } catch (error) {
-      return (error);
-   }
+interface IQueryromm {
+    room: bigint;
+}
+
+interface IHeaders {
+    'h-Custom': string;
+}
+
+interface IReply {
+    200: { success: boolean };
+    302: { url: string };
+    '4xx': { error: string };
+}
+
+server.get<{
+    Querystring: IQueryromm,
+    Headers: IHeaders,
+    Reply: IReply
+}>('/api/chat/', async (request, reply) => {
+    try {
+        const response = await fetch(`${APIchat}/?room=${request.query.room}`);
+        const result = await response.json();
+        return (result);
+    } catch (error) {
+        return (error);
+    }
 })
 
 server.get('/api/ping', async (request, reply) => {
-   return 'pong';
+    return 'pong';
 })
 
 server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
-   if (err) {
-      console.error(err);
-      process.exit(1);
-   }
-   console.log(`Server listening at ${address}`);
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
 })
--- srcs/back/chat-service/asset/srcs/index.ts
+++ srcs/back/chat-service/asset/srcs/index.ts
@@ -2,14 +2,38 @@ import fastify from 'fastify'
 
 const server = fastify();
 
-server.get('/test', async (request, reply) => {
-  return {message: 'good chat'};
+interface IQueryromm {
+    room: bigint;
+}
+
+interface IHeaders {
+    'h-Custom': string;
+}
+
+interface IReply {
+    200: { success: boolean };
+    302: { url: string };
+    '4xx': { error: string };
+}
+
+server.get<{
+    Querystring:    IQueryromm,
+    Headers:        IHeaders,
+    Reply:          IReply
+}>('/', async (request, reply) => {
+    let message: string;
+
+    if (request.query.room == undefined)
+        message = 'no room found';
+    else
+        message = `recv room ${request.query.room}`;
+    return {message};
 })
 
 server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`ready`);
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`ready`);
 })
