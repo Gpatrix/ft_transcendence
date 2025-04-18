ad1aad53c56c0e0b67546d80dae18ada6f4d140d
lchauvet <lchauvet@student.42lehavre.fr>
2025-04-18 11:36:58 +0200

    WIP: not working websocket

--- srcs/back/chat-service/asset/srcs/index.ts
+++ srcs/back/chat-service/asset/srcs/index.ts
@@ -1,20 +1,39 @@
 import fastify from 'fastify'
+import websocketPlugin from '@fastify/websocket'
+import WebSocket from 'ws';
+import type { RawData } from 'ws'; // <-- Import this from 'ws'
 
 const server = fastify();
+server.register(websocketPlugin);
 
 interface IChatInfo {
-    target: bigint;
+    target?: bigint;
 }
 
-server.get<{Querystring: IChatInfo,}>('/', async (request, reply) => {
-    let message: string;
+// server.get<{Querystring: IChatInfo,}>('/', async (request, reply) => {
+//     let message: string;
 
-    if (request.query.target == undefined)
-        message = 'no room found';
-    else
-        message = `recv room ${request.query.target}`;
-    return {message};
-})
+//     if (request.query.target == undefined)
+//         message = 'no room found';
+//     else
+//         message = `recv room ${request.query.target}`;
+//     return {message};
+// })
+
+
+// WebSocket route
+server.get('/', { websocket: true }, (socket: WebSocket, req) => {
+    try {
+        socket.on('message', (message: WebSocket.RawData) => {
+            console.log('Received:', message.toString());
+            socket.send('Hello Fastify WebSockets');
+          });
+    } catch (error) {
+        console.log(error);
+    }
+   
+  });
+
 
 server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
     if (err) {
