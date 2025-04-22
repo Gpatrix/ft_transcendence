28d4fa5e8b393a0c57a88191ea050e25d0481a15
Mame <134452452+YoungMame@users.noreply.github.com>
2025-04-22 10:25:16 +0200

    add:(generate code and send mail functions)

---
+++ srcs/back/auth-service/asset/srcs/mail.ts
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: process.env.MAIL_SERVICE,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.MAIL_SECURE,
+    auth: {
+      user:  process.env.MAIL_ADRESS,
+      pass:  process.env.MAIL_PASSWORD,
+    },
+  });
+
+async function sendMail(to: string, subject: string, text: string) {
+    const mailOptions = {
+        from: process.env.MAIL_ADRESS,
+        to,
+        subject,
+        text,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+    } catch (error) {
+        // lohg error
+    }
+};
+
+module.exports = sendMail;
