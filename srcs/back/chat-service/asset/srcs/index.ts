6546d3e68f8dd2d51c33b17b9b61c82756bf8222
lchauvet <lchauvet@student.42lehavre.fr>
2025-04-23 15:22:55 +0200

    WIP: token validation

--- srcs/back/chat-service/asset/srcs/index.ts
+++ srcs/back/chat-service/asset/srcs/index.ts
@@ -1,30 +1,33 @@
 import fastify from 'fastify';
-import websocketPlugin from '@fastify/websocket';
-import WebSocket from 'ws';
 import jwt from 'jsonwebtoken';
+import { WebSocketServer } from 'ws';
+import cookiesPlugin from '@fastify/cookie'
+
 
 const server = fastify();
-server.register(websocketPlugin);
+server.register(cookiesPlugin);
 server.register(wstest);
 
-interface structID
+const wss = new WebSocketServer({ noServer: true });
+
+interface structTarget
 {
     target: string;
-    token: string;
 }
 
 server.addHook('preValidation'
-   , (request, reply) => {
-   const token = request.headers['jwt'];
+   , (request, reply, done) => {
+      
+      const token = request.cookies.ft_transcendence_jw_token
       try
       {
          if (!token || token === undefined)
             return (reply.status(401).send({ error: "user_not_logged_in" }));
          const decoded = jwt.verify(token, process.env.JWT_SECRET);
-         console.log("debug");
          const id = decoded.data?.id;
          if (!id || id === undefined)
             return (reply.status(401).send({ error: "invalid_token_provided" }));
+         done();
       }
       catch (error) {
          console.log(error);
@@ -34,20 +37,23 @@ server.addHook('preValidation'
 
 async function wstest()
 {
-   server.get<{Params: structID}>('/api/chat/:target', { websocket: true }, (socket: WebSocket, req) => {
-      socket.send(`WebSockets target ${req.params.target}`);
+   server.get<{Params: structTarget}>('/api/chat/:target', function (request, reply)
+   {
+      console.log(`target: ${request.params.target}`);
+      reply.status(200).send({ response: "good" });
+      // socket.send(`WebSockets target ${req.params.target}`);
 
-      try
-      {
-         socket.on('message', (message: WebSocket.RawData) => {
-            console.log('Received:', message.toString());
-            socket.send(`bonce ${message}`);
-         });
-      }
-      catch (error)
-      {
-         console.log(error);
-      }
+      // try
+      // {
+      //    socket.on('message', (message: WebSocket.RawData) => {
+      //       console.log('Received:', message.toString());
+      //       socket.send(`bonce ${message}`);
+      //    });
+      // }
+      // catch (error)
+      // {
+      //    console.log(error);
+      // }
    });
 }
 
