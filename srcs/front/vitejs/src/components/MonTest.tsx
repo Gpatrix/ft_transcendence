5124b40f03fe81d20bb89b64ae9389399b8f81a2
cbeaufil <cbeaufil@localhost.localdomain>
2025-04-28 16:11:07 +0200

    ADD: saving components

--- srcs/front/vitejs/src/components/MonTest.tsx
+++ srcs/front/vitejs/src/components/MonTest.tsx
@@ -3,9 +3,9 @@ import { clsx } from 'clsx'
 import { Link } from "react-router-dom";
 import ProfilePic from './ProfilePic.tsx'
 // import { HomeIcon } from '@heroicons/react/solid'
-import React, { useState } from 'react';
 import arrowDown from './down-arrow-box.svg';
-import styles from './MonTest.module.css';
+
+
 
   
 type MonTestProps = {
@@ -19,80 +19,36 @@ type MonTestProps = {
 // recuperer toutes les infos de la partie avec l'api
 // depuis le parent ?
 
-export default function MonTest({ states, className, users } : MonTestProps) {
-
-    const [expendedItem, setExpendedItem] = useState(false); // pour garder l'élément sélectionné
-
-    const toggleMatch = () => {
-        console.log(expendedItem);
-        setExpendedItem(!expendedItem);
-        console.log(styles);
-        
-    }
-
+export default function MonTest({ states, className } : MonTestProps) {
     return (
-        <div  className={clsx('relative w-full text-yellow overflow-hidden border-solid border-yellow rounded-sm', states, className, expendedItem?styles.expended:'')}>
+        <div  className={clsx('relative w-full text-yellow ' + states, className)}>
             {/* <aside className='border-2 border-solid border-yellow rounded-sm mr-px'>date</aside> */}
-            <div className=' transform rotate-10 bg-yellow w-35 h-90 absolute z-1 -mt-5 -ml-10'></div>
+            {/* <div className=' transform rotate-10 bg-yellow w-30 h-30'></div> */}
             <div className=' flex shadow-2xs border-2 p-[10px]
-             w-full relative z-2'>
-                <span className='p-[10px] pr-[30px] states text-2xl text-black'>
+            border-solid border-yellow rounded-sm w-full'>
+                <span className='p-[20px] states text-2xl'>
                     {states}
                 </span>
-                <span className={clsx('flex justify-center items-baseline  h-min', styles.scores)}>
-                    
-                        {/* liste : */}
-
-                    <span className={clsx('flex flex-col', styles.users)}>
-                        {/* absolute top-2 */}
-                        <div className={clsx('flex md:gap-8 content-between main-cara max-h-1/1 mr-5 relative', styles.matchRecap)}>
+                <span className='scores flex justify-center items-center'>
+                    {/* <span className='users'> */}
+                        <span className='flex md:gap-8 content-between main-cara max-h-1/1'>
                             {/* faire attention a chaque utilisateurs */}
-                            <span className='w-[60px]'>
-                                <ProfilePic profileLink='https://tailwindcss.com/docs/height' image='https://localhost/test.jpeg'/>
-                            </span>
-
-                            <span className='separator text-5xl h-min self-center'>/</span>
+                            <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' size={0}/>
+                            <span className='separator'>/</span>
                             {/* faire attention : utiliser le tab passe en parametre */}
                             <span className='otherUsers  flex'>
-                                {
-                                    users.map((x, i) =>
-                                    {
-                                        if (i == 0)
-                                            return ''
-                                        console.log(i);
-                                        return <ProfilePic key={i} profileLink='https://www.google.com' image='https://localhost/test.jpeg' className={(i == 1?'':'ml-[-20px] ')+'inline-block w-[60px]'}/>
-                                    }
-                                    )
-                                }
-                                {/* <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' className='inline-block w-[60px]'/>
-                                <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' className='ml-[-20px] inline-block w-[60px]'/>
-                                <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' className='ml-[-20px] inline-block w-[60px]'/> */}
+                                <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' size={0}/>
+                                <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' size={0} className='ml-[-20px]'/>
+                                <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg' size={0} className='ml-[-20px]'/>
                             </span>
-                            <span className={clsx('placement h-min self-center ml-2', styles.test)}>
-                                1/4
-                            </span>
-                        </div>
-                        {
-                            users.map((x, i) =>
-                                <div key={i} className={clsx('flex md:gap-8 content-between main-cara max-h-1/1 mr-5 mt-3', styles.user)}>
-                                    {/* faire attention a chaque utilisateurs */}
-                                    <span className='w-[60px]'>
-                                        <ProfilePic profileLink='https://www.google.com' image='https://localhost/test.jpeg'/>
-                                    </span>
 
-                                    <span className='separator h-min self-center'>{x}</span>
-                                    <span className={clsx('placement h-min self-center ml-2')}>
-                                        pts
-                                    </span>
-                                </div>
-                            )
-
-                        }
+                        </span>
+                    {/* </span> */}
+                    <span className='placement'>
+                        1/4
                     </span>
-
-                    
-                    <span className={clsx('h-8 ml-2', styles.more)}>
-                        <img className='w-8' onClick={() => toggleMatch()} src={arrowDown} />
+                    <span className='more'>
+                        <img className='w-30 h-30' src={arrowDown} />
                     </span>
                     
                 </span>
--- srcs/front/vitejs/src/components/ProfilePic.tsx
+++ srcs/front/vitejs/src/components/ProfilePic.tsx
@@ -22,15 +22,17 @@ type ProfilePicProps = {
     image?: string;
     profileLink: string
     status?: VariantProps<typeof statusVariants>['status'];
+    size?: number;
 }
 
-export default function ProfilePic({ image, className, profileLink, status='none'} : ProfilePicProps) {
+export default function ProfilePic({ image, className, profileLink, status='none', size=30 } : ProfilePicProps) {
+    console.log("test" + size);
+    
     return (
         <Link to={profileLink}
-            className={clsx('relative max-w-fit h-full' //flex flex-col justify-center 
+            className={clsx('relative flex flex-col justify-center max-w-fit h-full'
                 // + (size>0?(" w-10 h-" + size):""), className)}
-                // + (size>0?(" w-" + size + " h-" + size):"")
-                , className)}
+                + (size>0?(" w-" + size + " h-" + size):""), className)}
             // className={clsx('relative' + (size>0?(" w-" + size + " h-" + size):""), className)}
         >
         { status != 'none' &&
@@ -39,7 +41,7 @@ export default function ProfilePic({ image, className, profileLink, status='none
             </span> }
             {
                 // image ? <img src={image} className="w-auto h-full rounded-full" />
-                image ? <img src={image} className="rounded-full h-1/1 inline-block" />
+                image ? <img src={image} className="rounded-full h-1/1" />
                       : <div className="flex justify-center items-center text-[100%] font-bold rounded-full h-1/1 w-1/1 bg-light-yellow text-grey">T</div>
             }
         </Link>
