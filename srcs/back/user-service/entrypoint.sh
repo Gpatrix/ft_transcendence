303285e981b3440e384f6deed23abf2350036155
Gap <132090220+Gpatrix@users.noreply.github.com>
2025-04-30 16:31:13 +0200

    37 chat socket add direct msg via db (#55)
    * WIP: including prisma
    
    * WIP: unique token
    
    * WIP: start of db handeling
    
    * WIP: pre-testing db
    
    * WIP: seting up db
    
    * WIP|error: channel not  dound in prisma
    
    * WIP: checking channel checking
    
    * ADD|WIP: msg creation on db
    
    * ADD|WIP: .dockerignore
    
    * ADD|WIP: setup docker
    
    * ADD|WIP: msg handle
    
    * WIP: refreshe
    
    * fix: default message for unknow action + hex -> base64 for hash
    
    * fix isblocked + verif isblocked in msg
    
    * fix: last error msg
    
    * fix: prisma schema
    
    * del: package-lock

--- srcs/back/user-service/entrypoint.sh
+++ srcs/back/user-service/entrypoint.sh
@@ -3,11 +3,11 @@
 cd ./asset
 
 if [ "$NODE_ENV" = "dev" ]; then
-    npm install;
+    npm install && npm cache clean --force;
     npm run db:dev;
     exec npm run dev
 else
-    npm install --omit=dev;
+    npm install --omit=dev && npm cache clean --force;
     npm run build
     npm run db:deploy
     exec npm run start;
