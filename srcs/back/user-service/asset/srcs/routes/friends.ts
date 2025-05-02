7cd693b328b2715f3f212b9311f5790ab7a02d5f
YoungMame <mame.duvey@gmail.com>
2025-05-02 06:40:40 -0400

    add:(basic friend request)

---
+++ srcs/back/user-service/asset/srcs/routes/friends.ts
@@ -0,0 +1,228 @@
+import { FastifyInstance } from 'fastify';
+import prisma from '../config/prisma';
+import jwt from 'jsonwebtoken';
+import { User } from '@prisma/client';
+
+
+function friendsRoute(server: FastifyInstance, options: any, done: any)
+{
+    interface postUserFriendRequestParams 
+    {
+        name: string
+    }
+
+    server.post<{ Params: postUserFriendRequestParams }>('/api/user/friends/requests/:name', async (request: any, reply: any) => {
+        try {
+            const targetName = request.params?.name;
+            const token = request.cookies['ft_transcendence_jw_token'];
+            if (!token)
+                return reply.status(403).send({ error: "0403" });
+            const decoded = jwt.decode(token);
+            const id = decoded?.data?.id;
+            if (!id)
+                return reply.status(403).send({ error: "0403" });
+            const user = await prisma.user.findUnique({
+                where: { 
+                    id: Number(id),
+                }
+            })
+            if (!user)
+                return reply.status(404).send({ error: "0403" });
+            const target = await prisma.user.findUnique({
+                where: { 
+                    name: targetName
+                }
+            })
+            const isAlreadyFriend = await prisma.friend.findFirst({
+                where: {
+                    userId: user.id,
+                    friendUserId: target.id
+                }
+            })
+            if (!target || isAlreadyFriend)
+                return reply.status(404).send({ error: "2012" });
+            if (target.id == user.id)
+                return reply.status(401).send({ error: "2011" });
+            const existingFriendRequest = await prisma.friendRequest.findFirst({
+                where: {
+                    authorId: user.id,
+                    targetId: target.id
+                }
+            })
+            if (existingFriendRequest)
+                return reply.status(429).send({ error: '2013'})
+            await prisma.friendRequest.create({
+                data: {
+                    author: { connect: { id: user.id } },
+                    target: { connect: { id: target.id } }
+                }
+            })
+            reply.status(201).send();
+        } catch (error) {
+            console.log(error)
+            return reply.status(500).send({ error: "0500" });
+        }
+    })
+
+    interface acceptUserFriendRequestParams 
+    {
+        id: string
+    }
+
+    server.put<{ Params: acceptUserFriendRequestParams }>('/api/user/friends/requests/:id', async (request: any, reply: any) => {
+        try {
+            const requestId = request.params?.id;
+            const token = request.cookies['ft_transcendence_jw_token'];
+            if (!token)
+                return reply.status(403).send({ error: "0403" });
+            const decoded = jwt.decode(token);
+            const id = decoded?.data?.id;
+            if (!id)
+                return reply.status(403).send({ error: "0403" });
+            let user = await prisma.user.findUnique({
+                where: { 
+                    id: Number(id)
+                },
+                include: {
+                    friends: true
+                }
+            })
+            if (!user)
+                return reply.status(404).send({ error: "0403" });
+            const existingFriendRequest = await prisma.friendRequest.findFirst({
+                where: {
+                    id: Number(requestId)
+                }
+            })
+            if (!existingFriendRequest)
+                return reply.status(404).send({ error: '0404'});
+            let author = await prisma.user.findUnique({
+                where: { 
+                    id: existingFriendRequest.authorId
+                },
+                include: {
+                    friends: true
+                }
+            })
+            if (!author)
+                return reply.status(404).send({ error: "2012" });
+            if (existingFriendRequest.targetId != id)
+                return reply.status(401).send({ error: "0401" });
+            await prisma.friendRequest.delete({
+                where: {
+                    id: existingFriendRequest.id
+                },
+            });
+
+            await prisma.friend.create({
+                data: {
+                  user: { connect: { id: user.id } },
+                  friendUserId: author.id
+                }
+            });
+              
+            await prisma.friend.create({
+                data: {
+                    user: { connect: { id: author.id } },
+                    friendUserId: user.id
+                }
+            });
+            reply.status(201).send();
+        } catch (error) {
+            console.log(error);
+            return reply.status(500).send({ error: "0500" });
+        }
+    })
+
+    interface getUserFriendsParams 
+    {
+        id: string
+    }
+
+    interface getUserFriendsBody
+    {
+        credential: string
+    }
+
+    server.post<{ Params: getUserFriendsParams, Body: getUserFriendsBody }>('/api/user/getFriends/:id', async (request: any, reply: any) => {
+        try {
+            const params = request.params;
+            const body = request.body;
+            const credential = body?.credential;
+            if (!credential || credential != process.env.API_CREDENTIAL)
+                return reply.status(401).send({ error: "0401" });
+            let user: User | null = null;
+            user = await prisma.user.findUnique({
+                where: { 
+                    id: Number(params.id)
+                },
+                include: {
+                    friends: true
+                }
+            })
+            if (!user)
+                return reply.status(404).send({ error: "0404" });
+            reply.send(user.friends);
+        } catch (error) {
+            console.log(error);
+            return reply.status(500).send({ error: "0500" });
+        }
+    })
+
+    server.get<{}>('/api/user/friends', async (request: any, reply: any) => {
+        try {
+            const token = request.cookies['ft_transcendence_jw_token'];
+            if (!token)
+                return reply.status(403).send({ error: "0403" });
+            const decoded = jwt.decode(token);
+            const id = decoded?.data?.id;
+            if (!id)
+                return reply.status(403).send({ error: "0403" });
+            let user: User | null = null;
+            user = await prisma.user.findUnique({
+                where: { 
+                    id: Number(id)
+                },
+                include: {
+                    friends: true
+                }
+            })
+            if (!user)
+                return reply.status(404).send({ error: "0404" });
+            reply.send(user.friends);
+        } catch (error) {
+            console.log(error);
+            return reply.status(500).send({ error: "0500" });
+        }
+    });
+
+    server.get<{}>('/api/user/receivedFriendRequests', async (request: any, reply: any) => {
+        try {
+            const token = request.cookies['ft_transcendence_jw_token'];
+            if (!token)
+                return reply.status(403).send({ error: "0403" });
+            const decoded = jwt.decode(token);
+            const id = decoded?.data?.id;
+            if (!id)
+                return reply.status(403).send({ error: "0403" });
+            let user: User | null = null;
+            user = await prisma.user.findUnique({
+                where: { 
+                    id: Number(id)
+                },
+                include: {
+                    receivedFriendRequests: true
+                }
+            })
+            if (!user)
+                return reply.status(404).send({ error: "0404" });
+            reply.send(user.receivedFriendRequests);
+        } catch (error) {
+            console.log(error);
+            return reply.status(500).send({ error: "0500" });
+        }
+    });
+    done();
+}
+
+module.exports = friendsRoute;
