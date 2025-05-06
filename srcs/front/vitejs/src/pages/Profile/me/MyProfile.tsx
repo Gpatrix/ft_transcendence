05af0246de4c7af219422b4c5c73688b72710842
Lixhr <127848043+Lixhr@users.noreply.github.com>
2025-05-06 17:21:13 +0200

    77 profile fetch and display users informations (#85)
    * DONE: Routes modified
    
    * FIX: Dumb type error on user lang
    
    * DONE: Language / users infos change
    
    * DONE: User pp display
    
    * DONE: ok, waiting for more test cases
    
    ---------
    
    Co-authored-by: cbeaufil <cbeaufil@localhost.localdomain>

--- srcs/front/vitejs/src/pages/Profile/me/MyProfile.tsx
+++ srcs/front/vitejs/src/pages/Profile/me/MyProfile.tsx
@@ -1,23 +1,63 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import LeftPart from "../LeftPart"
 import RightPart from "../RightPart"
+import { useAuth } from "../../../AuthProvider"
+import { useParams } from "react-router"
 
-function getUserParams() {
-    fetch("https://localhost/api/user/get_profile/")
+export type ProfileDataType = {
+    name: string
+    email: string | null
+    bio: string | null
+    profPicture: string | null
+    rank : number
+    lang : number | null
 }
 
-
 export default function MyProfile() {
+    const [profileData, setProfileData] = useState<ProfileDataType>({
+        name: "",
+        email: null,
+        bio: null,
+        profPicture: null,
+        rank: 0,
+        lang : null
+    })
 
+    const { fetchWithAuth } = useAuth();
+    const { id } = useParams();
+
+    function getUserParams() {
+        fetchWithAuth(`https://localhost/api/user/get_profile/${id != "me" && id}`)
+            .then((response) => response.json())
+            .then((json) => {
+                const data = json.data
+                setProfileData(prev => ({
+                    ...prev,
+                    name: data.name,
+                    email: data.email,
+                    bio: data.bio,
+                    profPicture: data.profPicture,
+                    rank: data.rank,
+                    lang: data.lang
+                }));
+            })
+            .catch((error) => {
+                console.error("Error :", error);
+            });
+    }
+
+    useEffect(() => {
+        console.log(profileData)
+    }, [profileData]);
 
     useEffect(()=>{
         getUserParams()
     }, [])
 
     return (
-        <div className="lg:w-full ml-auto mr-auto h-fit flex justify-stretch z-1 lg:flex-row flex-col">
-            <LeftPart />
-            <RightPart />
+        <div className="px-5 w-full ml-auto mr-auto h-fit flex justify-stretch z-1 lg:flex-row flex-col">
+            <LeftPart data={profileData}/>
+            <RightPart data={profileData}/>
         </div>
     )
 }
