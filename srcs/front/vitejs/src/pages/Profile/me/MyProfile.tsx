9761458d5b0bb0ebd6e7b8a88901aab2128ac820
Lixhr <127848043+Lixhr@users.noreply.github.com>
2025-05-06 09:24:18 +0200

    FIX: Google-auth wrong behavior, force email on login (#75)
    Co-authored-by: cbeaufil <cbeaufil@localhost.localdomain>

--- srcs/front/vitejs/src/pages/Profile/me/MyProfile.tsx
+++ srcs/front/vitejs/src/pages/Profile/me/MyProfile.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from "react"
 import LeftPart from "../LeftPart"
 import RightPart from "../RightPart"
 
+function getUserParams() {
+    fetch("https://localhost/api/user/get_profile/")
+}
+
+
 export default function MyProfile() {
+
+
+    useEffect(()=>{
+        getUserParams()
+    }, [])
+
     return (
         <div className="lg:w-full ml-auto mr-auto h-fit flex justify-stretch z-1 lg:flex-row flex-col">
             <LeftPart />
