ea19dce8a1885364b730d1569191ddac2a3cf211
lchauvet <lchauvet@student.42lehavre.fr>
2025-05-06 15:03:10 +0200

    add|WIP: newChannel

--- srcs/back/chat-service/asset/srcs/index.ts
+++ srcs/back/chat-service/asset/srcs/index.ts
@@ -5,7 +5,7 @@ import websocketPlugin, { WebsocketHandler } from '@fastify/websocket';
 import WebSocket from 'ws';
 import { PrismaClient } from "../prisma/prisma_client";
 import axios, { AxiosError } from 'axios';
-import { PrismaClientUnknownRequestError } from '@prisma/client/runtime/library';
+import { Param } from '@prisma/client/runtime/library';
 
 
 const prisma = new PrismaClient();
@@ -13,7 +13,7 @@ const prisma = new PrismaClient();
 const server = fastify();
 server.register(cookiesPlugin);
 server.register(websocketPlugin);
-server.register(chatws);
+server.register(chat_api);
 
 interface payloadstruct
 {
@@ -40,8 +40,8 @@ server.addHook('preValidation'
       {
          if (!token || token === undefined)
             return (reply.status(403).send({ error: "403" }));
-         const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-         const id = decoded.data?.id;
+         const decoded: tokenStruct = jwt.verify(token, process.env.JWT_SECRET as string).data;
+         const id = decoded.id;
          if (!id || id === undefined)
             return (reply.status(403).send({ error: "403" }));
          done();
@@ -88,55 +88,67 @@ async function is_blocked(by: number, target: number): Promise<string>
 interface t_channel
 {
    id: number;
-   isGroup: boolean;
+   isGame: boolean;
    createdAt: Date;
 }
 
-async function GC_private_Channel(userId1: number, userId2: number): Promise<t_channel | string>
+async function CreateChannel(usersID: number[], isGame: boolean): Promise<t_channel | string>
+{
+   usersID.sort((a, b) => a - b)
+
+   try
+   {
+      const newChannel = await prisma.channel.create({
+         data: {
+            isGame: isGame,
+            participants: {
+               create: usersID.map((userId) => ({
+                  userId: userId,
+               })),
+            },
+         },
+         include: {
+            participants: true,
+         },
+      });
+      
+      return (newChannel);
+   }
+   catch (error)
+   {
+      if (axios.isAxiosError(error))
+         {
+            console.log(error.response?.data);
+            if (error.response?.data.error !== undefined)
+               return (error.response?.data.error);
+         }
+      return ("0503");
+   }
+}
+
+async function findChannel(usersID: number[], isGame: boolean): Promise<t_channel | string | null>
 {
    try
    {
+      usersID.sort((a, b) => a - b)
+
       const existingChannel = await prisma.channel.findFirst({
          where: {
-           isGroup: false,
-           participants: {
-             every: {
-               OR: [
-                 { userId: userId1 },
-                 { userId: userId2 }
-               ]
-             }
-           }
-         },
+            isGame: isGame,
+            participants: {
+               some: {
+                  userId: {
+                     in: usersID,
+                  },
+               },
+            },
+         }, 
          include: {
-           participants: true,
-         }
-       });
-     
-       if (
-         existingChannel &&
-         existingChannel.participants.some(p => p.userId === userId1) &&
-         existingChannel.participants.some(p => p.userId === userId2)
-       ) {
-         return existingChannel as t_channel;
-       }
-       
-      const newChannel = await prisma.channel.create({
-      data: {
-         isGroup: false,
-         participants: {
-            create: [
-               { userId: userId1 },
-               { userId: userId2 }
-               ]
-            }
-      },
-      include: {
-         participants: true
-      }
+            participants: true,
+         },
       });
-      
-      return newChannel as t_channel;
+
+      return (existingChannel);
    }
    catch (error: AxiosError | unknown)
    {
@@ -148,7 +160,7 @@ async function GC_private_Channel(userId1: number, userId2: number): Promise<t_c
       }
       return ("0503");
    }
- }
+}
 
 interface userInfo
 {
@@ -210,7 +222,9 @@ async function handle_msg(payload: payloadstruct, token: tokenStruct, socket: We
       return;
    }
 
-   const channel: t_channel | string = await GC_private_Channel(token.id, target_user.id);
+   let channel: t_channel | string | null = await findChannel([token.id, target_user.id], false);
+   if (channel === null)
+      channel = await CreateChannel([token.id, target_user.id], false);
    if (typeof channel === 'string')
       return (socket.send(channel));
 
@@ -259,7 +273,7 @@ async function handle_refresh(payload: payloadstruct, token: tokenStruct, socket
       if (typeof target_info === 'string')
          return (socket.send(`{"error": ${target_info}}`));
 
-      const channel: t_channel | string = await GC_private_Channel(token.id, target_info.id);
+      const channel: t_channel | string = await findOrCreateChannel(token.id, target_info.id);
       if (typeof channel === 'string')
          return (socket.send(`{"error": ${target_info}}`));
 
@@ -298,15 +312,25 @@ function data_handler(
 
       case "refresh":
          handle_refresh(payload, token, socket);
-
          break;
+
       default:
          socket.send(`{"error": 0400}`);
          return;
    }
 }
 
-async function chatws()
+interface newChannelParams
+{
+   userId: number[];
+}
+
+interface newChannelBody
+{
+    credential: string
+}
+
+async function chat_api()
 {
    server.get('/api/chat/connect', {websocket: true}, (socket: WebSocket, request) => 
    {
@@ -327,7 +351,18 @@ async function chatws()
          console.log(error);
       }
    });
-}
+
+   server.post<{ Params: newChannelParams, Body: newChannelBody}>('/api/chat/newChannel', async (request, reply) => {
+      const credential = request.body?.credential;
+      if (!credential || credential != process.env.API_CREDENTIAL)
+         reply.status(401).send({ error: "private_route" });
+   
+      let channel: t_channel | string= await CreateChannel(request.params.userId, true);
+      if (typeof channel === 'string')
+         return (reply.status(400).send(channel));
+      
+      return (reply.status(200).send({channelId: channel.id}));
+   })}
 
 server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) =>
 {
