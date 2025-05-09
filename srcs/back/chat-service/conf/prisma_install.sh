8e4fb2e12a949cd31819ecb687f3dab5b415c081
lchauvet <lchauvet@student.42lehavre.fr>
2025-05-09 12:39:54 +0200

    WIP: new file Utils + new doker build form

--- srcs/back/chat-service/conf/prisma_install.sh
+++ srcs/back/chat-service/conf/prisma_install.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
+
 npx prisma generate
 
 if [ "$NODE_ENV" = "dev" ]; then
     npx prisma migrate dev --name init
 else
     npx prisma migrate deploy
-fi
-
-npm run build;
-
-if [ "$NODE_ENV" = "dev" ]; then
-    exec npm run dev
-else
-    exec npm run start;
 fi
