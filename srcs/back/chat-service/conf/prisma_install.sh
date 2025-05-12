4bb004960a4d75bbc2f594189b5da18accbd6b41
lchauvet <lchauvet@student.42lehavre.fr>
2025-05-12 10:31:16 +0200

    WIP: auth-service finish

--- srcs/back/chat-service/conf/prisma_install.sh
+++ srcs/back/chat-service/conf/prisma_install.sh
@@ -2,3 +2,6 @@
 
 npx prisma generate
 npx prisma migrate dev --name init
+
+#  "db:deploy": "npx prisma generate && npx prisma migrate deploy && npx prisma db seed",
+#  "db:dev": "npx prisma generate && npx prisma db push && npx prisma db seed"
