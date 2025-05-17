ef664860c1d7f695479caa850ee41acdfcee3051
YoungMame <mame.duvey@gmail.com>
2025-05-17 09:22:53 +0200

    add:(blockchain service)

---
+++ srcs/back/blockchain-service/asset/srcs/index.ts
@@ -0,0 +1,57 @@
+import fastify from 'fastify'
+import rateLimitPlugin from '@fastify/rate-limit';
+
+const server = fastify();
+
+server.register(require('@fastify/static'), {
+  root: '/usr/src/upload-service/uploads',
+  prefix: '/api/upload/'
+})
+
+server.register(require('@fastify/multipart'), {
+  limits: {
+    fieldNameSize: 100, // Max field name size in bytes
+    fieldSize: 100,     // Max field value size in bytes
+    fields: 10,         // Max number of non-file fields
+    fileSize: 1050000,  // For multipart forms, the max file size in bytes
+    files: 1,           // Max number of file fields
+    headerPairs: 2000,  // Max number of header key=>value pairs
+    parts: 1000         // For multipart forms, the max number of parts (fields + files)
+  }
+});
+
+server.register(rateLimitPlugin, {
+  max: 100,
+  timeWindow: '1 minute',
+  allowList: ['127.0.0.1']
+});
+
+server.register(require("./routes/upload"), { config: {
+  max: 5,
+  timeWindow: '1 minute'
+}});
+
+async function main() {
+  let _address;
+  await server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    _address = address;
+    console.log(`Server listening at ${_address}`);
+  })
+}
+
+main();
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
+
---
+++ srcs/back/blockchain-service/asset/srcs/routes/upload.ts
@@ -0,0 +1,55 @@
+import { FastifyInstance } from "fastify";
+import fs from 'fs';
+import pump from 'pump';
+
+function uploadRoutes (server: FastifyInstance, options: any, done: any)
+{
+
+    interface uploadPostBody {
+        file: any,
+        credential: string
+    }
+    
+    server.post<{ Body: uploadPostBody }>('/api/upload/', async (req: any, res: any) => {
+        try {
+            const file = await req.file();
+            const parts = await req.parts();
+            const credential = file.fields.credential.value;
+            if (credential != process.env.API_CREDENTIAL)
+                return (res.status(401).send({ error: "private_route" }));  // private route (:
+            if (!file)
+                return (res.status(420).send({ error: "no_file_uploaded" }));
+            const fileName = `${Date.now()}_${file.filename}`;
+            const storedFile = fs.createWriteStream(`../uploads/${fileName}`);
+            pump(file.file, storedFile);
+            res.status(200).send({ fileName: fileName });
+        } catch (error) {
+            res.status(500).send({ error: "0500" });
+        }
+    });
+
+    interface uploadDeleteBody {
+        credential: string
+    }
+
+    interface uploadDeleteParams {
+        fileName: string
+    }
+
+    server.delete<{ Body: uploadDeleteBody, Params: uploadDeleteParams}>('/api/upload/:fileName', async (req: any, res: any) => {
+        try {
+            const credential = req.body.credential;
+            const fileName = req.params.fileName
+            if (credential != process.env.API_CREDENTIAL)
+                return (res.status(401).send({ error: "private_route" }));  // private route (:
+            fs.unlink(`../uploads/${fileName}`, err => {});
+            res.status(200).send({ message: 'file_successfully_deleted' });
+        } catch (error) {
+            res.status(500).send({ error: "0500" });
+        }
+    });
+
+    done()
+}
+
+module.exports = uploadRoutes;
