7109d11159917506d905a477e18d09f3f0010bfc
YoungMame <mame.duvey@gmail.com>
2025-05-17 11:15:00 +0200

    add:(main files)

--- srcs/back/blockchain-service/asset/srcs/index.ts
+++ srcs/back/blockchain-service/asset/srcs/index.ts
@@ -1,30 +1,6 @@
 import fastify from 'fastify'
-import rateLimitPlugin from '@fastify/rate-limit';
 
 const server = fastify();
-
-server.register(require('@fastify/static'), {
-  root: '/usr/src/upload-service/uploads',
-  prefix: '/api/upload/'
-})
-
-server.register(require('@fastify/multipart'), {
-  limits: {
-    fieldNameSize: 100, // Max field name size in bytes
-    fieldSize: 100,     // Max field value size in bytes
-    fields: 10,         // Max number of non-file fields
-    fileSize: 1050000,  // For multipart forms, the max file size in bytes
-    files: 1,           // Max number of file fields
-    headerPairs: 2000,  // Max number of header key=>value pairs
-    parts: 1000         // For multipart forms, the max number of parts (fields + files)
-  }
-});
-
-server.register(rateLimitPlugin, {
-  max: 100,
-  timeWindow: '1 minute',
-  allowList: ['127.0.0.1']
-});
 
 server.register(require("./routes/upload"), { config: {
   max: 5,
