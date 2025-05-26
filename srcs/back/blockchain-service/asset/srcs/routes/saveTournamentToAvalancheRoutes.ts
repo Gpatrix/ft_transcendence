ad246e36f4057a336d77cbd5d3e3586829e1811b
Maël Duvey <mduvey@MacBook-Air-de-Mael.local>
2025-05-26 08:34:26 +0200

    fix:(deploy in local with hardhad node first signer)

--- srcs/back/blockchain-service/asset/srcs/routes/saveTournamentToAvalancheRoutes.ts
+++ srcs/back/blockchain-service/asset/srcs/routes/saveTournamentToAvalancheRoutes.ts
@@ -7,7 +7,7 @@ const HARDHAT_RPC_URL = "http://0.0.0.0:8545/";
 const factoryAddress = addressData.value;
 const factoryABI = abiData.abi;
 let     rpcURL : string = process.env.FUJI_RPC_URL || "https://api.avax-test.network/ext/bc/C/rpc";
-let     owner = process.env.SIGNER_PRIVATE_KEY || "0x;
+let     owner = process.env.SIGNER_PRIVATE_KEY || "0x";
 
 if (process.env.NODE_ENV == "dev")
 {
@@ -24,13 +24,13 @@ function saveTournamentToAvalancheRoutes (server: FastifyInstance, options: any,
             const wallet = new ethers.Wallet(owner, provider);
 
             // this create a contract instance connected to the provider
-            const factory = new ethers.Contract(factoryAddress, factoryABI, provider);
-            console.log("factory", factory);
-            console.log(factory.interface.fragments.map(f => f.name));
+            const factory = new ethers.Contract(factoryAddress, factoryABI, wallet);
+            ethers.formatet
+            const balance = await provider.getBalance(wallet.address);
+            console.log("balance: ", ethers.utils.formatEther(balance));
             const tx = await factory.deployTournament(1);
-            console.log("tx", tx);
             const receipt = await tx.wait();
-            console.log("receipt", receipt);
+            console.log("balance after deploy: ", await provider.getBalance(wallet.address).formatEther());
             res.send({ status: "ok", txHash: receipt.transactionHash });
         } catch (error) {
             console.error("Error ", error);
