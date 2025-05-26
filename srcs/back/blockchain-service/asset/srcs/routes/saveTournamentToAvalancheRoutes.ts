d731546081f27d4f27dd454df57510ccf4c49c1b
Mame <134452452+YoungMame@users.noreply.github.com>
2025-05-26 10:31:15 +0200

    add:(refacto and deploy tournament in bc route)

--- srcs/back/blockchain-service/asset/srcs/routes/saveTournamentToAvalancheRoutes.ts
+++ srcs/back/blockchain-service/asset/srcs/routes/saveTournamentToAvalancheRoutes.ts
@@ -15,23 +15,45 @@ if (process.env.NODE_ENV == "dev")
     owner = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // hardhat local signers[0]
 }
 
+function getProvider(): ethers.JsonRpcProvider
+{    
+    const provider = new ethers.JsonRpcProvider(rpcURL);
+    return provider;
+}
+
+function getDeployer(): ethers.Wallet
+{
+    const provider = getProvider();
+    return new ethers.Wallet(owner, provider);
+}
+
+function getFactory(): ethers.Contract
+{
+    const wallet = getDeployer();
+    return new ethers.Contract(factoryAddress, factoryABI, wallet);
+}
+
+async function deployTournament(tournamentId: number): Promise<ethers.ContractTransactionReceipt>
+{
+    const factory = getFactory();
+    const tx = await factory.deployTournament(1);
+    const receipt = await tx.wait();    
+    console.log(receipt);
+    return (receipt);
+}
+
+
+
 function saveTournamentToAvalancheRoutes (server: FastifyInstance, options: any, done: any)
 {
-    
-    server.get('/api/blockchain/ping', async (req: any, res: any) => {
-        try {
-            const provider = new ethers.JsonRpcProvider(rpcURL);
-            const wallet = new ethers.Wallet(owner, provider);
+    interface deployTournamentParams {
+        tournamentId: number;
+    }
 
-            // this create a contract instance connected to the provider
-            const factory = new ethers.Contract(factoryAddress, factoryABI, wallet);
-            ethers.formatet
-            const balance = await provider.getBalance(wallet.address);
-            console.log("balance: ", ethers.utils.formatEther(balance));
-            const tx = await factory.deployTournament(1);
-            const receipt = await tx.wait();
-            console.log("balance after deploy: ", await provider.getBalance(wallet.address).formatEther());
-            res.send({ status: "ok", txHash: receipt.transactionHash });
+    server.get<{ Params: deployTournamentParams }>('/api/blockchain/deploy/:tournamentId', async (req: any, res: any) => {
+        try {
+            const deployTransactionReceipt = await deployTournament(req.params.tournamentId);
+            res.send({ status: "ok", txHash: deployTransactionReceipt.hash, contractAddress: deployTransactionReceipt.to });
         } catch (error) {
             console.error("Error ", error);
             res.status(500).send({ error: "0500" });
