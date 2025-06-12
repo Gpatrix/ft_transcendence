f8d0d96d1d9016780aa0110263eea9cbee1a55fa
Mame <134452452+YoungMame@users.noreply.github.com>
2025-06-12 10:31:39 +0200

    fix:(image upload) (#317)

--- srcs/front/asset/src/pages/Profile/LeftPart.tsx
+++ srcs/front/asset/src/pages/Profile/LeftPart.tsx
@@ -77,6 +77,7 @@ export default function LeftPart({ data, owner }: LeftPartProps) {
                 window.location.reload();
             } else {
                 const data = await response.json();
+                console.log('data', data)
                 setError(get_server_translation(data.error));
                 setFile(undefined)
             }
