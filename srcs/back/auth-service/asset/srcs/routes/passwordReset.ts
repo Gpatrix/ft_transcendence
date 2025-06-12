987498d7f180e0e57ab896c772eef3af876ca26b
Mael <mame.duvey@gmail.com>
2025-06-12 10:04:32 +0200

    fix:(password reset and google params)

--- srcs/back/auth-service/asset/srcs/routes/passwordReset.ts
+++ srcs/back/auth-service/asset/srcs/routes/passwordReset.ts
@@ -47,6 +47,38 @@ export default function passwordResetRoutes(server: FastifyInstance, options: an
         }
     });
 
+    // interface passwordResetSubmitBody {
+    //     token: string,
+    //     password:string
+    // }
+
+    // server.post<{ Body: passwordResetSubmitBody }>('/api/auth/passwordReset/submit', {}, async (req, res) => {
+    //     try {
+    //         const password = req.body.password;
+    //         const token = req.body.token;
+    //         const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+    //         const email = decoded.data.email;
+    //         if (!email)
+    //             return res.status(230).send({ error: "1016" });
+    //         const newPassword = await bcrypt.hash(password, 12);
+    //         const userPasswordUpdate = await fetch(`http://user-service:3000/api/user/password/${email}`,
+    //         {
+    //             method: 'PUT',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify({
+    //                 credential: process.env.API_CREDENTIAL,
+    //                 password: newPassword,
+    //             }),
+    //         }); 
+    //         const data = await userPasswordUpdate.json();
+    //         if (!userPasswordUpdate.ok)
+    //             return res.status(userPasswordUpdate.status).send({ error: "1016"})
+    //         res.status(200).send();
+    //     } catch (error) {
+    //         res.status(230).send({ error: "1016" });
+    //     }
+    // });
+
     interface passwordResetSubmitBody {
         token: string,
         password:string
@@ -54,13 +86,36 @@ export default function passwordResetRoutes(server: FastifyInstance, options: an
 
     server.post<{ Body: passwordResetSubmitBody }>('/api/auth/passwordReset/submit', {}, async (req, res) => {
         try {
-            const password = req.body.password;
+            const actual_password = req.body.actual_password;
+            const new_password = req.body.new_password;
             const token = req.body.token;
             const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
             const email = decoded.data.email;
             if (!email)
                 return res.status(230).send({ error: "1016" });
-            const newPassword = await bcrypt.hash(password, 12);
+            const response = await fetch(`http://user-service:3000/api/user/lookup/${email}`,
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: email,
+                    credential: process.env.API_CREDENTIAL
+                }),
+            });
+            const data = await response.json();
+            console.log('data', data)
+            if (response.status != 200)
+                res.status(response.status).send({ error: data.error})
+            const user = data;
+            if (!user)
+                return res.status(230).send({ error: "1006" });
+            if (!user.password)
+                return res.status(230).send({ error: "1014" });
+            const isCorrect = await bcrypt.compare(actual_password as string, user.password);
+            console.log('isCorrect', isCorrect)
+            if (!isCorrect)
+                return res.status(230).send({ error: "1006"});
+            const newPassword = await bcrypt.hash(new_password, 12);
             const userPasswordUpdate = await fetch(`http://user-service:3000/api/user/password/${email}`,
             {
                 method: 'PUT',
@@ -70,8 +125,7 @@ export default function passwordResetRoutes(server: FastifyInstance, options: an
                     password: newPassword,
                 }),
             }); 
-            const data = await userPasswordUpdate.json();
-            if (!userPasswordUpdate.ok)
+            if (userPasswordUpdate.status != 200)
                 return res.status(userPasswordUpdate.status).send({ error: "1016"})
             res.status(200).send();
         } catch (error) {
